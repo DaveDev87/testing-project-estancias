2612df9a2b95e6ffd0762356ae5130509cb326b3
dave <alexruedarivas@gmail.com>
2022-05-26 16:41:29 -0500

    lista de cosas por hacer v1

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,99 @@
 <template>
   <main id="app">
     <section class="header">
-      <h1>Hola Mundo!</h1>
-      <p>Esta es mi lista de cosas por hacer</p>
+      <h1>{{ title }}</h1>
+      <p>{{ subtitle }}</p>
     </section>
-    <section class="task__container">
+    <section>
       <div class="card">
-        <h1>Pasear al perro</h1>
-        <p>Tengo que sacar a blackie por que si no se aburre y tira las macetas</p>
-      </div>
-      <div class="card">
-        <h1>Lavar el carro</h1>
-        <p>Para que no llegue algun random a escribir "lavame con la ventana"</p>
-      </div>
-      <div class="card">
-        <h1>Preparar la comida</h1>
-        <p>Porque hay que seguir viviendo y el delivery esta muy caro</p>
+        <div>Nombre: {{ user.name }}</div>
+        <div>Edad: {{ user.age }}</div>
+        <span class="puede_pasar" v-if="user.age >= 18">Pasele!</span>
+        <span class="no_puede_pasar" v-else>No pasa</span>
       </div>
     </section>
+    <!-- <section class="task__container">
+      <div v-for="(task, index) in tasks" :key="index">
+        <div v-if="task.completed === false" class="card">
+          <div class="task__date">{{ task.date }}</div>
+          <h1>{{ task.title }}</h1>
+          <span>{{ task.author }}</span>
+          <p>{{ task.description }}</p>
+        </div>
+      </div>
+    </section> -->
   </main>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
+  name: "App",
+  // mounted() {
+  //   const body = {
+  //     email: "alexruedarivas@gmail.com",
+  //     user: "David A. Rueda",
+  //     sendEmailToUser: true,
+  //   };
 
-}
+  //   fetch(
+  //     "https://4519cjvuci.execute-api.us-east-1.amazonaws.com/prod/convocatoria-evaluador/emailalert",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(body),
+  //       headers: new Headers({
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Origin:": "*"
+  //       }),
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log("unu", err));
+  // },
+  data() {
+    return {
+      title: "Hola amigos",
+      subtitle: "Lista de cosas para hacer:",
+
+      user: {
+        name: "Pepe",
+        age: 20,
+        active: true,
+      },
+
+      // tasks: [
+      //   {
+      //     title: "Lavar la ropa",
+      //     description: "Para no oler mal",
+      //     author: "David",
+      //     date: "2022-05-1",
+      //     completed: true,
+      //   },
+      //   {
+      //     title: "lavar los platos",
+      //     description: "Para no enfermarse",
+      //     date: "2022-05-1",
+      //     author: "Sherlyn",
+      //     completed: true,
+      //   },
+      //   {
+      //     title: "Hacer la tarea",
+      //     description: "Porque si no me reprueba Joel",
+      //     date: "2022-05-1",
+      //     author: "Cris",
+      //     completed: false,
+      //   },
+      // ],
+    };
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
 
 #app {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -43,13 +102,25 @@ export default {
   margin-top: 60px; */
 }
 
-.header{
+.header {
   text-align: center;
 }
 
 .task__container {
   display: flex;
+  flex-wrap: wrap;
+}
 
+.task__date {
+  text-align: right;
+}
+
+.puede_pasar {
+  color: green;
+}
+
+.no_puede_pasar {
+  color: red;
 }
 
 .card {
